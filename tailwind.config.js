0e3a95ee2f6d3fcbbad95c20872ca3b63167d0f0
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-05-08 16:46:24 +0530

    config file editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        top: '0 -4px 4px rgba(0, 0, 0, 0.1)',
+        right: '4px 0 4px rgba(0, 0, 0, 0.1)',
+        left: '-4px 0 4px rgba(0, 0, 0, 0.1)',
+      },
+    },
+    
   },
   plugins: [],
 }
