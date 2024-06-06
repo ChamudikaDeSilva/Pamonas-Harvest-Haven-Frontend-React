015214e0ba5494c3f8fec102ade3372f624e8539
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-06-06 17:16:17 +0530

    address.jsx  edited

--- src/components/commonComponent/address.jsx
+++ src/components/commonComponent/address.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const TopInfo = () => (
-  <div className="top-info flex flex-wrap justify-between items-center px-4 py-1 bg-lime-500 lg:w-full lg:mx-auto lg:ml-auto sm:px-8 md:px-16 xl:px-24">
+  <div className="top-info sticky flex flex-wrap justify-between items-center px-4 py-1 bg-lime-500 w-3/4 rounded-[16px]">
     <div className="flex items-center space-x-4">
       <div className="flex items-center">
-        <i className="fas fa-map-marker-alt text-secondary"></i>
-        <a href=" " className="text-white text-sm ml-2">123 Street, New York</a>
+        <i className="fas fa-map-marker-alt text-secondary hover:text-amber-500"></i>
+        <a href=" " className="text-white text-sm ml-2 hover:text-amber-500">123 Street, New York</a>
       </div>
       <div className="flex items-center">
-        <i className="fas fa-envelope text-secondary"></i>
-        <a href=" " className="text-white text-sm ml-2">Email@Example.com</a>
+        <i className="fas fa-envelope text-secondary hover:text-amber-500"></i>
+        <a href=" " className="text-white text-sm ml-2 hover:text-amber-500">Email@Example.com</a>
       </div>
     </div>
     <div className="top-link flex items-center space-x-4">
-      <a href=" " className="text-white text-sm">Privacy Policy</a>
+      <a href=" " className="text-white text-sm hover:text-amber-500">Privacy Policy</a>
       <span className="text-white">/</span>
-      <a href=" " className="text-white text-sm">Terms of Use</a>
+      <a href=" " className="text-white text-sm hover:text-amber-500">Terms of Use</a>
       <span className="text-white">/</span>
-      <a href=" " className="text-white text-sm">Sales and Refunds</a>
+      <a href=" " className="text-white text-sm hover:text-amber-500">Sales and Refunds</a>
     </div>
   </div>
 );
