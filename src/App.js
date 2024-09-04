bf0b0b966b0f5658f89f700e0c01af8924822349
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-09-04 14:54:13 +0530

    DealsCombo in shop page

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
                   <>
                     <ShopHero />
                     <ShopPageContent />
+                    <DealsCombos />
                     <Testimonials />
                   </>
                 }
